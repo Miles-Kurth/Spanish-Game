1f099513bce4eb86dd140b11d8d36378afd0d6b0
Miles Kurth <mkurth9612@student.pps.net>
2026-03-04 15:15:55 -0800

    55

--- game.js
+++ game.js
@@ -247,7 +247,7 @@ function startGame() {
     }
 
     //Event listeners
-    document.getElementsByName("canvas").addEventListener("click", function(e) {
+    this.canvas.addEventListener("click", function(e) {
         const rect = gameArea.canvas.getBoundingClientRect();
         const mouseX = e.clientX - rect.left;
         const mouseY = e.clientY - rect.top;
